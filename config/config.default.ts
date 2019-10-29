765fb2cd88982936b6b58dda8dfc71a571b2404d
superbear <superbear@DN030.local>
2019-10-29 09:49:40 +0800

    init

---
+++ config/config.default.ts
@@ -0,0 +1,32 @@
+import { EggAppConfig, EggAppInfo, PowerPartial } from "egg";
+
+export default (appInfo: EggAppInfo) => {
+  const config = {} as PowerPartial<EggAppConfig>;
+
+  // override config from framework / plugin
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + "_1572306875118_9361";
+
+  // add your egg config in here
+  config.middleware = [];
+
+  // databases cofig
+  config.sequelize = {
+    dialect: "mysql",
+    host: "127.0.0.1",
+    password: "",
+    port: 3306,
+    database: "egg-sequelize-doc-default"
+  };
+
+  // add your special config in here
+  const bizConfig = {
+    sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`
+  };
+
+  // the return config will combines to EggAppConfig
+  return {
+    ...config,
+    ...bizConfig
+  };
+};
---
+++ database/migrations/init_database.sql
@@ -0,0 +1,59 @@
+DROP TABLE if exists `acl_privilege`.`pri_user`;
+CREATE TABLE acl_privilege.pri_user (
+  `id` BIGINT(32) UNSIGNED NOT NULL AUTO_INCREMENT comment '主键id',
+  `user_id` CHAR(32) NOT NULL comment '用户id',
+  `user_uuid` CHAR(32) NOT NULL comment '用户uuid',
+  `create_time` DATETIME NOT NULL COMMENT '创建时间',
+  `update_time` DATETIME NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_user_id`(`user_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户信息表';
+
+DROP TABLE if exists `acl_privilege`.`pri_user_role`;
+CREATE TABLE acl_privilege.pri_user (
+  `id` BIGINT(32) UNSIGNED NOT NULL AUTO_INCREMENT comment '主键id',
+  `user_id` CHAR(32) NOT NULL comment '用户id',
+  `role_id` CHAR(16) UNSIGNED NOT NULL comment '角色id',
+  `create_time` DATETIME NOT NULL COMMENT '创建时间',
+  `update_time` DATETIME NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_user_role_id`(`user_id`,`role_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户角色关系表';
+
+DROP TABLE if exists `acl_privilege`.`pri_role`;
+CREATE TABLE `acl_privilege`.`pri_user` (
+  `id` BIGINT(32) UNSIGNED NOT NULL AUTO_INCREMENT comment '主键id',
+  `role_id` CHAR(16) UNSIGNED NOT NULL comment '角色id',
+  `role_name` VARCHAR(128) UNSIGNED NOT NULL comment '角色id',
+  `create_time` DATETIME NOT NULL COMMENT '创建时间',
+  `update_time` DATETIME NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_role_id`(`role_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
+
+DROP TABLE if exists `acl_privilege`.`pri_role_privilege`;
+CREATE TABLE acl_privilege.pri_user (
+  `id` BIGINT(32) UNSIGNED NOT NULL AUTO_INCREMENT comment '主键id',
+  `role_id` CHAR(16) UNSIGNED NOT NULL comment '角色id',
+  `privilege_id` CHAR(16) UNSIGNED NOT NULL comment '权限id',
+  `create_time` DATETIME NOT NULL COMMENT '创建时间',
+  `update_time` DATETIME NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_role_privilege_id`(`role_id`,`privilege_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色权限关系表';
+
+DROP TABLE if exists `acl_privilege`.`pri_privilege`;
+CREATE TABLE `acl_privilege`.`pri_privilege` (
+  `id` BIGINT(32) UNSIGNED NOT NULL AUTO_INCREMENT comment '主键id',
+  `privilege_id` CHAR(16) UNSIGNED NOT NULL comment '权限id',
+  `privilege_name` VARCHAR(128) UNSIGNED NOT NULL comment '权限名称',
+  `uri` VARCHAR(128) UNSIGNED NOT NULL comment '权限接口',
+  `sn` CHAR(32) UNSIGNED NOT NULL comment 'uri md5值',
+  `create_time` DATETIME NOT NULL COMMENT '创建时间',
+  `update_time` DATETIME NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_privilege_id_uri`(`privilege_id`,`uri`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='权限表';
+
+
+
